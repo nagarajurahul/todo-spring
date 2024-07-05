c5ee825a751816f5f7087de237bbc140ede4e55b
Rahul Nagaraju <rahulnagaraju724@gmail.com>
2024-07-05 01:28:43 -0500

    Update TodoController to list todos

--- src/main/java/com/rnagaraju/springboot/todo/TodoController.java
+++ src/main/java/com/rnagaraju/springboot/todo/TodoController.java
@@ -1,15 +1,27 @@
 package com.rnagaraju.springboot.todo;
 
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class TodoController {
+	
+	TodoService todoService;
+	
+	public TodoController(TodoService todoService) {
+		super();
+		this.todoService=todoService;
+	}
 
 	@RequestMapping(value="list-todos")
 //	@ResponseBody
-	public String listAllTodos() {
+	public String listAllTodos(ModelMap model) {
+		List<Todo> todos=todoService.findByUsername("in28minutes");
+		model.put("todos", todos);
 		return "listTodos";
 	}
 }
