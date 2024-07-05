7a867b47c413bf305f144618ca4b9d4e95c84065
Rahul Nagaraju <rahulnagaraju724@gmail.com>
2024-07-05 01:17:18 -0500

    Update Todo.java

--- src/main/java/com/rnagaraju/springboot/todo/Todo.java
+++ src/main/java/com/rnagaraju/springboot/todo/Todo.java
@@ -52,5 +52,11 @@ public class Todo {
 	public void setDone(boolean done) {
 		this.done = done;
 	}
+
+	@Override
+	public String toString() {
+		return "Todo [id=" + id + ", username=" + username + ", description=" + description + ", dueDate=" + dueDate
+				+ ", done=" + done + "]";
+	}
 	
 }
