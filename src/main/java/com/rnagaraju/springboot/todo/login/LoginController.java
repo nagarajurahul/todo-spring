2c6a00ca684a70695cfbee2966fffb9401947a79
Rahul Nagaraju <rahulnagaraju724@gmail.com>
2024-07-04 23:41:21 -0500

    JSP configured for the project

--- src/main/java/com/rnagaraju/springboot/todo/login/LoginController.java
+++ src/main/java/com/rnagaraju/springboot/todo/login/LoginController.java
@@ -8,9 +8,9 @@ import org.springframework.web.bind.annotation.ResponseBody;
 public class LoginController {
 
 	@RequestMapping("login")
-	@ResponseBody
+//	@ResponseBody
 	public String goToLoginPage() {
-		return "This is Login Page";
+		return "login";
 	}
 	
 }
