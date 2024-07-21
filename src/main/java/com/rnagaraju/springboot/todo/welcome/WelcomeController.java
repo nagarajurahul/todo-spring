31ec6dccce8912bc63ab8e649a21ba6f848f35ea
Rahul Nagaraju <rahulnagaraju724@gmail.com>
2024-07-21 17:35:36 -0500

    Welcome Controller edited, Removed Login Controllers

--- src/main/java/com/rnagaraju/springboot/todo/welcome/WelcomeController.java
+++ src/main/java/com/rnagaraju/springboot/todo/welcome/WelcomeController.java
@@ -1,15 +1,17 @@
 package com.rnagaraju.springboot.todo.welcome;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class WelcomeController {
 
-	@RequestMapping("todo")
-	@ResponseBody
-	public String welcome() {
-		return "Welcome to To-Do";
+	@RequestMapping("/")
+//	@ResponseBody
+	public String welcome(ModelMap model) {
+		model.put("name","Rahul");
+		return "welcome";
 	}
 }
