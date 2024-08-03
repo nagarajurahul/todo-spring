318215c68ed1ae4253f9393226f3e66bddc5c62f
Rahul Nagaraju <rahulnagaraju724@gmail.com>
2024-08-03 14:04:02 -0500

    Update security methods and add additional users

--- src/main/java/com/rnagaraju/springboot/todo/security/SpringSecurityConfiguration.java
+++ src/main/java/com/rnagaraju/springboot/todo/security/SpringSecurityConfiguration.java
@@ -16,19 +16,29 @@ public class SpringSecurityConfiguration {
 	@Bean
 	public InMemoryUserDetailsManager createUserDetailsManager() {
 		
+		String username = "rahul";
+		String password = "dummy";
+		
+		UserDetails userDetails1 = createNewUser(username, password);
+		UserDetails userDetails2 = createNewUser("Rohit", "dummy");
+
+		
+		return new InMemoryUserDetailsManager(userDetails1, userDetails2);
+		
+	}
+
+	private UserDetails createNewUser(String username, String password) {
 		Function<String, String> passwordEncoder
 		=input -> passwordEncoder().encode(input);
 		//		UserDetails userDetails= User.withDefaultPasswordEncoder()
+		
 		UserDetails userDetails= User.builder()
 		.passwordEncoder(passwordEncoder)
-		.username("rahul")
-		.password("dummy")
+		.username(username)
+		.password(password)
 		.roles("USER","ADMIN")
 		.build();
-		
-		
-		return new InMemoryUserDetailsManager(userDetails);
-		
+		return userDetails;
 	}
 	
 	@Bean
